741ba46529fe17a69eff22452cf5d055f19fc5df
Travis CI User <travis@example.org>
2018-10-27 12:21:59 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES['aligned'] = {};
+ALIASES['bare_metal'] = {};
+ALIASES['cortex_m'] = {};
+ALIASES['cortex_m_semihosting'] = {};
+ALIASES['panic_semihosting'] = {};
+ALIASES['vcell'] = {};
+ALIASES['volatile_register'] = {};
